48da88fbded4361e43a18d0e3a8aea98bcd7aa29
IsaacEMCoral <imendozac1@est.ups.edu.ec>
2025-12-06 01:36:56 -0500

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>MercApp Frontend</h1>
+    <button @click="checkHealth">Probar conexión API</button>
+    <pre>{{ health }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const health = ref(null)
+const api = import.meta.env.VITE_API_URL
+
+const checkHealth = async () => {
+  const res = await fetch(`${api}/health`)
+  health.value = await res.json()
+}
+</script>
